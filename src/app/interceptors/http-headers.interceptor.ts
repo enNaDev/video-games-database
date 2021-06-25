2790526ea15fd15c9d41b5337bcfa581e134c0f6
Enis Natzi <natzien@aris-lux.lan>
2021-06-25 14:54:14 +0300

    Bug with the headers interceptor fixed

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -13,11 +13,11 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
     ): Observable<HttpEvent<any>> {
         req = req.clone({
             setHeaders: {
-                'x-rapidapi-key': '',
-                'x-rapidapi-host': '',
+                'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
             },
             setParams: {
-                key: ''
+                key: 'e40e743af2c94b0c916a8aa618fb4473'
             }
         });
 
